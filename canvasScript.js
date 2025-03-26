562dca8e374561520bc34e2c0be7874a8d16ea76
bigstitches <108193491+bigstitches@users.noreply.github.com>
2025-03-26 23:10:37 +0000

    Deploying to gh-pages from @ bigstitches/blazor-app@6eff6719d1184f3cbcb9ef45ce49a9c8c9e851b3 🚀

---
+++ canvasScript.js
@@ -0,0 +1,29 @@
+﻿function drawOnCanvas() {
+    var canvas = document.getElementById('myCanvas');
+    if (canvas.getContext) {
+        var ctx = canvas.getContext('2d');
+
+        // Clear the canvas
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        // Draw a rectangle
+        ctx.fillStyle = 'green';
+        ctx.fillRect(10, 10, 150, 100);
+
+        // Draw a circle
+        ctx.beginPath();
+        ctx.arc(300, 200, 50, 0, Math.PI * 2, true);
+        ctx.fillStyle = 'blue';
+        ctx.fill();
+    }
+}
+
+function eraseOnCanvas() {
+    var canvas = document.getElementById('myCanvas');
+    if (canvas.getContext) {
+        var ctx = canvas.getContext('2d');
+
+        // Clear the canvas
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+}
